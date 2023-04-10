b3f5e2018ff1639b74893b26c96fb27359a80e1e
GeorgeH <default_hyn@163.com>
2023-04-10 21:20:22 +0800

    feat: article [about]

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -124,14 +124,14 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: "鸡汤", link: "/other/soul/" },
         ],
       },
-      { text: "关于", link: "/about/" },
+      { text: "关于", link: "/pages/c5aa1f/" },
       {
         text: "收藏",
         link: "/pages/beb6c0bd8a66cea6/",
         items: [
           { text: "网站", link: "/pages/beb6c0bd8a66cea6/" },
-          { text: "资源", link: "/pages/eee83a9211a70f9d/" },
-          { text: "Vue资源", link: "/pages/12df8ace52d493f6/" },
+          { text: "前端轮子", link: "/pages/47cf96/" },
+         
         ],
       },
       {
@@ -228,9 +228,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 页脚信息
     footer: {
-      createYear: 2021, // 博客创建年份
+      createYear: 2023, // 博客创建年份
       copyrightInfo:
-        'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+        'GeorgeH | <a href="https://github.com/GeorgeHcc/GeorgeHcc.github.io/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
